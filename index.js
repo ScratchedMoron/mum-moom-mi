76629a701040232fc4a91b04270cfcb1a30df447
hotsoycandy <wkdwnsghd08@naver.com>
2020-03-11 01:33:39 +0000

    fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const CURSED_TYPES = [
 ]
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(helmet())
 app.use(express.static('public'))
 
